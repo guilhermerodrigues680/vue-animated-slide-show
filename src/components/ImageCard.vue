b8fd9b8b0b03125deb7280518b03a89db23bb370
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-06-25 06:02:20 -0300

    Criado LandingPage e ImageCard

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <img class="photo" :src="imgurl" alt="foto">
+    <h3>{{title}}</h3>
+    <p>{{text}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageCard',
+
+  props: {
+    imgurl: String,
+    title: String,
+    text: String,
+  },
+}
+</script>
+
+<style scoped>
+h3, p {
+  margin: 0;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  grid-template-rows: 4fr 1fr 1fr;
+  place-items: center;
+  height: 40%;
+  width: 50%;
+}
+
+.photo {
+  height: 100%;
+  width: auto;
+  object-fit: contain;
+  border-radius: 0.5rem;
+  vertical-align: middle;
+}
+</style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
-
+  <div class="page-container">
+    <ImageCard
+      :imgurl="imageList[currentImage].imgUrl"
+      :title="imageList[currentImage].title"
+      :text="imageList[currentImage].text"
+    />
   </div>
 </template>
 
 <script>
+import ImageCard from "@/components/ImageCard.vue";
+
 export default {
   name: 'LandingPage',
+
+  components: {
+    ImageCard,
+  },
+
+  data: () => ({
+    imageList: [
+      { imgUrl: require('@/assets/demo-img-1.jpg'), title: 'demo-img-1', text: 'Imagem demo 1' },
+      { imgUrl: require('@/assets/demo-img-2.jpg'), title: 'demo-img-2', text: 'Imagem demo 2' },
+      { imgUrl: require('@/assets/demo-img-3.jpg'), title: 'demo-img-3', text: 'Imagem demo 3' },
+      { imgUrl: require('@/assets/demo-img-4.jpg'), title: 'demo-img-4', text: 'Imagem demo 4' },
+    ],
+    currentImage: 0,
+  }),
 }
 </script>
 
 <style scoped>
-
+.page-container {
+  display: grid;
+  grid-template-rows: 100vh;
+  place-items: center;
+  background: #157bad url('~@/assets/night-sky.jpg') no-repeat;
+  background-size: cover;
+  color: white;
+  font-family: sans-serif;
+}
 </style>
