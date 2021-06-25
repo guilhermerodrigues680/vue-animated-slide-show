f3c21a8d7f3df868b119f2824c5823b3bd56af39
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-06-25 07:41:57 -0300

    Implementado vue transitions e animação de fundo

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="page-container">
-    <ImageCard
-      :imgurl="imageList[currentImage].imgUrl"
-      :title="imageList[currentImage].title"
-      :text="imageList[currentImage].text"
-    />
+    <transition
+      appear
+      name="fade"
+      mode="out-in"
+      @after-enter="afterEnter"
+      @after-appear="afterEnter"
+    >
+      <ImageCard class="image-card"
+        :imgurl="imageList[currentImage].imgUrl"
+        :title="imageList[currentImage].title"
+        :text="imageList[currentImage].text"
+        :key="currentImage"
+      />
+    </transition>
   </div>
 </template>
 
@@ -26,7 +35,23 @@ export default {
       { imgUrl: require('@/assets/demo-img-4.jpg'), title: 'demo-img-4', text: 'Imagem demo 4' },
     ],
     currentImage: 0,
+    intervalMs: 1000,
   }),
+
+  methods: {
+    afterEnter(/*el*/) {
+      console.debug('afterEnter()');
+      setTimeout(() => this.nextImage(), this.intervalMs);
+    },
+    nextImage() {
+      const imageListLastIdx = this.imageList.length - 1;
+      this.currentImage = this.currentImage + 1 > imageListLastIdx ? 0 : this.currentImage + 1;
+    },
+    previousImage() {
+      const imageListLastIdx = this.imageList.length - 1;
+      this.currentImage = this.currentImage - 1 < 0 ? imageListLastIdx : this.currentImage - 1;
+    },
+  }
 }
 </script>
 
@@ -35,9 +60,45 @@ export default {
   display: grid;
   grid-template-rows: 100vh;
   place-items: center;
-  background: #157bad url('~@/assets/night-sky.jpg') no-repeat;
-  background-size: cover;
+  background-color: #111111;
+  background-image: url('~@/assets/night-sky-texture.jpg');
+  /* background-image: linear-gradient(to top, #e0c3fc40, #8ec5fc40), url('~@/assets/night-sky-texture.jpg'); */
   color: white;
   font-family: sans-serif;
+  position: relative;
+  animation: night-sky-moving 30s linear infinite;
+}
+
+.page-container::before {
+  content: "";
+  z-index: 1;
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to top, #e0c3fc30, #8ec5fc30);
+  /* background: linear-gradient(to top, var(--from, #e0c3fc30), var(--to, #8ec5fc30)); */
+}
+
+.image-card {
+  z-index: 10;
+}
+
+@keyframes night-sky-moving {
+  to {
+    /* dimensões da textura */
+    background-position: 600px 600px;
+  }
+}
+
+/* ============ Vue Transition ============ */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
